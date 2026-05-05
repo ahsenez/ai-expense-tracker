e33cab9719b25795349bb77ee6c18661f4e6b5c8
ahsen ezgün <ahsen_ezgun@hotmail.com>
2026-05-05 16:17:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,4 @@ function addExpense() {
   const title = document.getElementById("title").value;
   const amount = Number(document.getElementById("amount").value);
   const category = document.getElementById("category").value;
+
