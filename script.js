d06bd9a5ea61b1b4d621fd51ea7a2d350c268784
ahsen ezgün <ahsen_ezgun@hotmail.com>
2026-05-05 16:15:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 function saveExpenses() {
   localStorage.setItem("expenses", JSON.stringify(expenses));
 }
+
+function addExpense() {
